cb29148cd72b855bfb402849138f37760cc452f8
Abhik Mitra <abmitra@microsoft.com>
2020-05-23 23:40:20 +0530

    Added Reference

--- EventHubDemo/EventHubConsumer/EventProcessor.cs
+++ EventHubDemo/EventHubConsumer/EventProcessor.cs
@@ -9,25 +9,25 @@ namespace EventHubConsumer
 {
     class SimpleEventProcessor : IEventProcessor
     {
-        Task IEventProcessor.CloseAsync(PartitionContext context, CloseReason reason)
+        public Task CloseAsync(PartitionContext context, CloseReason reason)
         {
             Console.WriteLine($"Processor Shutting Down. Partition '{context.PartitionId}', Reason: '{reason}'.");
             return Task.CompletedTask;
         }
 
-        Task IEventProcessor.OpenAsync(PartitionContext context)
+        public Task OpenAsync(PartitionContext context)
         {
             Console.WriteLine($"SimpleEventProcessor initialized. Partition: '{context.PartitionId}'");
             return Task.CompletedTask;
         }
 
-        Task IEventProcessor.ProcessErrorAsync(PartitionContext context, Exception error)
+        public Task ProcessErrorAsync(PartitionContext context, Exception error)
         {
             Console.WriteLine($"Error on Partition: {context.PartitionId}, Error: {error.Message}");
             return Task.CompletedTask;
         }
 
-        Task IEventProcessor.ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        public Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
         {
             foreach (var eventData in messages)
             {
